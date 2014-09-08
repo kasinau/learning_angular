2967561b8323d8f910ffb122080525c29cdf9059
Radu Pavelco <radu.pavelco@yopeso.com>
2014-09-08 08:00:15 +0300

    data

--- test.js
+++ test.js
@@ -19,6 +19,23 @@ function DeathrayMenuController($scope){
     }
 }
 
+function HeaderController($scope){
+    $scope.isError = false;
+    $scope.isWarning = false;
+
+    $scope.showError = function(){
+        $scope.messageText = 'This is an error';
+        $scope.isError = true;
+        $scope.isWarning = false;
+    }
+
+    $scope.showWarning = function(){
+        $scope.messageText = 'This is an warning';
+        $scope.isError = false;
+        $scope.isWarning = true;
+    }
+}
+
 function RestaurantTableController($scope){
     $scope.directory = [
         {
